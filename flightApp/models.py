000ad7d755adf3a9a03bfce9eddd3db8c37b8909
LeinardB92 <71524074+LeinardB92@users.noreply.github.com>
2022-12-08 15:03:15 -0600

    Create custom validator

--- flightApp/models.py
+++ flightApp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Flight(models.Model):
     flightNumber=models.CharField(max_length=10)
-    operatingAirlines=models.CharField(max_length=20)
+    operatingAirlines=models.CharField(max_length=20, blank = True, null = True)
     departureCity=models.CharField(max_length=20)
     arrivalCity=models.CharField(max_length=20)
     dateOfDeparture=models.DateField()
